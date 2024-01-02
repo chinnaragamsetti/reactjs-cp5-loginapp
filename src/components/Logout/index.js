a161de84b09c60088c8cb8abe795fb7d5dd99b1a
chinnaragamsetti <110217256+chinnaragamsetti@users.noreply.github.com>
2024-01-02 10:58:49 +0530

    Update index.js

--- src/components/Logout/index.js
+++ src/components/Logout/index.js
@@ -3,12 +3,14 @@
 import './index.css'
 import LogoutMessage from '../Message/'
 
-const Logout=()=>
-    return(
+const Logout = () =>
+ return(
     <div>
-        <LogoutMessage/>
-        <button className='button' type='button' onClick={this.Logout}>Logout</button>
+        <LogoutMessage />
+        <button className="button" type="button" onClick={this.Logout}>
+        Logout
+        </button>
     </div>
-    );
+    )
 
 export default Logout
